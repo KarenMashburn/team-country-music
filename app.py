1dc6441a4a72aac648d2406a94ffc2e33596bece
bromul <mtthw.mldn@gmail.com>
2021-08-18 19:30:28 -0400

    Add function to attempt conversion of pitch_type to array

--- app.py
+++ app.py
@@ -19,16 +19,26 @@ def predict_pitch():
 
 @app.route('/predict', methods=['POST'])
 def prediction(pitch_input):
-    features = np.array(pitch_input).reshape(1, 23)
+    features = np.array(pitch_input)
     result = model.predict(features)
     return result[0]
+
+def type_encoder(pitch_type):
+    vals = []
+    for i in range(0, 14):
+        vals[i] = 0
+    vals[pitch_type] = 1
+    return vals
 
 
 @app.route('/result', methods=['POST'])
 def result():
     if request.method == 'POST':
         pitch_input = request.form.to_dict()
+        vals = type_encoder(pitch_input['pitch_type'])
+        pitch_input.pop('pitch_type')
         pitch_input = list(pitch_input.values())
+        pitch_input.append(vals)
         result = prediction(pitch_input)
         if str(result) == 'S':
         # if int(result) > .5:
@@ -36,11 +46,6 @@ def result():
         else:
             pitch_prediction = 'BALL (Or Maybe a hit)!'
         return pitch_prediction
-
-
-
-
-
 
 
 if __name__ == '__Home__':
