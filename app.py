9576786a7a780173239e8c4bcc870be7f2e0bd91
bromul <mtthw.mldn@gmail.com>
2021-08-18 18:06:44 -0400

    Define pitch_input and reshape input

--- app.py
+++ app.py
@@ -5,30 +5,36 @@ import pickle
 app = Flask(__name__)
 model = pickle.load(open('all_pitchers_gbc_pitch.sav', 'rb'))
 
+# Home page
 @app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route('/result', methods=['POST'])
-def result():
-    if request.method == 'POST':
-        pitch_input = list(pitch_input.values())
-        result = prediction(pitch_input)
-        # if str(result) == S:
-        if int(result) > .5:
-            prediction = 'STEEEEERIKE'
-        else:
-            prediction = 'BALL (Or Maybe a hit)!'
-        return prediction
-
+# Predict-a-Pitch page
 @app.route('/predict')
 def predict_pitch():
     return render_template('pitches.html')
 
 def prediction(pitch_input):
-    features = np.array(pitch_input)
+    features = np.array(pitch_input).reshape(1, -1)
     result = model.predict(features)
     return result[0]
+
+
+@app.route('/result', methods=['POST'])
+def result():
+    if request.method == 'POST':
+        pitch_input = request.form.to_dict()
+        pitch_input = list(pitch_input.values())
+        result = prediction(pitch_input)
+        if str(result) == 'S':
+        # if int(result) > .5:
+            pitch_prediction = 'STEEEEERIKE'
+        else:
+            pitch_prediction = 'BALL (Or Maybe a hit)!'
+        return pitch_prediction
+
+
 
 
 
