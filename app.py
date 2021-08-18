1d214b154505fcbe721bbbadf5ac81e7f4253710
bromul <mtthw.mldn@gmail.com>
2021-08-17 21:48:33 -0400

    Attempt to make button do a thing

--- app.py
+++ app.py
@@ -3,12 +3,22 @@ from flask import Flask, request, render_template
 import pickle
 
 app = Flask(__name__)
-model = pickle.load(open('all_pitchers_zone.sav', 'rb'))
+model = pickle.load(open('all_pitchers_gbc_pitch.sav', 'rb'))
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
+@app.route('/result', methods=['POST'])
+def result():
+    if request.method == 'POST':
+        pitch_input = list(pitch_input.values())
+        result = prediction(pitch_input)
+        if int(result) > .5:
+            prediction = 'STEEEEERIKE'
+        else:
+            prediction = 'BALL (Or Maybe a hit)!'
+        return prediction
 
 @app.route('/predict')
 def predict_pitch():
@@ -19,27 +29,8 @@ def prediction(pitch_input):
     result = model.predict(features)
     return result[0]
 
-def result():
-    if request.method == 'POST':
-        pitch_input = list(pitch_input.values())
-        result = Prediction(pitch_input)
-        if int(result) > .5:
-            prediction = 'STEEEEERIKE'
-        else:
-            prediction = 'BALL (Or Maybe a hit)!'
-        return prediction
 
 
-# @app.route('/result', methods=['POST'])
-# def result():
-#     if request.method == 'POST':
-#         pitch_input = list(pitch_input.values())
-#         result = Prediction(pitch_input)
-#         if int(result) > .5:
-#             prediction = 'STEEEEERIKE'
-#         else:
-#             prediction = 'BALL (Or Maybe a hit)!'
-#         return prediction
 
 
 if __name__ == '__Home__':
