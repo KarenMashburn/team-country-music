099a07f3674c933c1a9d01849df89ba57bba9260
bromul <mtthw.mldn@gmail.com>
2021-08-18 20:15:58 -0400

    Continue to work on model predictions

--- app.py
+++ app.py
@@ -17,29 +17,40 @@ def predict_pitch():
     return render_template('pitches.html')
 
 
+# Prediction and encoding functions
 @app.route('/predict', methods=['POST'])
 def prediction(pitch_input):
-    features = np.array(pitch_input)
+    features = np.array(pitch_input).reshape(1, -1)
     result = model.predict(features)
     return result[0]
 
-def type_encoder(pitch_type):
+def type_encoder(p_type):
     vals = []
     for i in range(0, 14):
-        vals[i] = 0
-    vals[pitch_type] = 1
+        vals.append(0)
+    # vals[p_type] = 1
     return vals
 
 
+# Predict from model and present result
 @app.route('/result', methods=['POST'])
 def result():
     if request.method == 'POST':
+
+        # Collect and clean input data
         pitch_input = request.form.to_dict()
         vals = type_encoder(pitch_input['pitch_type'])
         pitch_input.pop('pitch_type')
         pitch_input = list(pitch_input.values())
-        pitch_input.append(vals)
+
+        # Append pitch_type values to pitch_input
+        for value in vals:  
+            pitch_input.append(value)
+
+        # Predict result
         result = prediction(pitch_input)
+
+        # Display result
         if str(result) == 'S':
         # if int(result) > .5:
             pitch_prediction = 'STEEEEERIKE'
